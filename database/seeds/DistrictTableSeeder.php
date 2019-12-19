7d154a03981828fd28bbb77d19b5e95b18b6dfad
quyenmanucian98 <ledanhquyen1998@gmail.com>
2019-12-19 11:48:54 +0700

    refactor database

---
+++ database/seeds/DistrictTableSeeder.php
@@ -0,0 +1,309 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DistrictTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $district = new \App\District();
+        $district->city_id = 1;
+        $district->name = 'Hoài Đức';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 1;
+        $district->name = 'Đông Anh';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 1;
+        $district->name = 'Thanh Trì';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 1;
+        $district->name = 'Đan Phượng';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 1;
+        $district->name = 'Ba Vì';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 1;
+        $district->name = 'Thường Tín';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 2;
+        $district->name = 'Từ Sơn';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 2;
+        $district->name = 'Tiên Du';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 2;
+        $district->name = 'Quế Võ';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 2;
+        $district->name = 'Yên Phong';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 2;
+        $district->name = 'Gia Bình';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 2;
+        $district->name = 'Lương Tài';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 3;
+        $district->name = 'Sơn Dương';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 3;
+        $district->name = 'Yên Sơn';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 3;
+        $district->name = 'Hàm Yên';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 3;
+        $district->name = 'Chiêm Hóa';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 3;
+        $district->name = 'Na Hang';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 3;
+        $district->name = 'Lâm Bình';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 4;
+        $district->name = 'Đại Từ';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 4;
+        $district->name = 'Định Hóa';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 4;
+        $district->name = 'Đồng Hỷ';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 4;
+        $district->name = 'Phú Bình';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 4;
+        $district->name = 'Phú Lương';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 4;
+        $district->name = 'Võ Nhai';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 5;
+        $district->name = 'Bắc Hà';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 5;
+        $district->name = 'Bảo Thắng';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 5;
+        $district->name = 'Bảo Yên';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 5;
+        $district->name = 'Bát Xát';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 5;
+        $district->name = 'Sa Pa';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 5;
+        $district->name = 'Mường Khương';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 6;
+        $district->name = 'Lục Yên';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 6;
+        $district->name = 'Mù Cang Chải';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 6;
+        $district->name = 'Trạm Tấu';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 6;
+        $district->name = 'Trấn yên';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 6;
+        $district->name = 'Văn Chấn';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 6;
+        $district->name = 'Văn Yên';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 7;
+        $district->name = 'Cẩm Phả';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 7;
+        $district->name = 'Đông Triều';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 7;
+        $district->name = 'Hà Cối';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 7;
+        $district->name = 'Hoành Bồ';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 7;
+        $district->name = 'Móng Cái';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 7;
+        $district->name = 'Đầm Hà';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 8;
+        $district->name = 'Kim Thành';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 8;
+        $district->name = 'Kinh Môn';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 8;
+        $district->name = 'Nam Sách';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 8;
+        $district->name = 'Ninh Giang';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 8;
+        $district->name = 'Thanh Hà';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 8;
+        $district->name = 'Bình Giang';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 9;
+        $district->name = 'Đà Bắc';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 9;
+        $district->name = 'Mai Châu';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 9;
+        $district->name = 'Lương Sơn';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 9;
+        $district->name = 'Kim Bôi';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 9;
+        $district->name = 'Cao Phong';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 9;
+        $district->name = 'Kỳ Sơn';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 10;
+        $district->name = 'Bình Lục';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 10;
+        $district->name = 'Duy Tiên';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 10;
+        $district->name = 'Lý Nhân';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 10;
+        $district->name = 'Kim Bảng';
+        $district->save();
+
+        $district = new \App\District();
+        $district->city_id = 10;
+        $district->name = 'Thanh Liêm';
+        $district->save();
+    }
+}
