388f57dd1954e5701c329d336e0d5de16d06fcde
duyduong65 <tg.bluesky66@gmail.com>
2019-12-20 15:04:50 +0700

    refactor controller rent house

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\House');
     }
+
+    public function orders()
+    {
+        return $this->hasMany('App\Order');
+    }
 }
